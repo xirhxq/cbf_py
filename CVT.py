75cd034ed99842a60ba449af4a01775f5391eac9
xirhxq <373836275@qq.com>
2023-04-11 00:10:16 +0800

    change CVT dependency into scipy

--- CVT.py
+++ CVT.py
@@ -1,34 +1,49 @@
 from shapely.geometry import Polygon, Point
-import pyvoro
+from scipy.spatial import Voronoi, voronoi_plot_2d
 import numpy as np
-from scipy.spatial import Voronoi
 
 
 def cal_cvt(points, world):
     points = [Point(p) for p in points]
-    points = [p for p in points if world.contains(p)]
+    # points = [p for p in points if world.contains(p)]
     points = [p.coords[0] for p in points]
     points = np.array(points)
+    # print('original points', points)
     bbox = np.array(world.bounds).reshape(2, 2).T
-    cells = pyvoro.compute_2d_voronoi(points, bbox, 0.1)
-    vertices = [cell['vertices'] for cell in cells]
-    polygons = []
-    for v in vertices:
-        polygons.append(Polygon(v))
-    # polygons = [p.intersection(world) for p in polygons if world.contains(p)]
-    return polygons
-
-
-def voronoi_polygons_scipy(points, world):
-    points = [Point(p) for p in points]
-    points = [p for p in points if world.contains(p)]
-    points = [p.coords[0] for p in points]
-    points = np.array(points)
+    x_min, x_max = bbox[0]
+    y_min, y_max = bbox[1]
+    x_mid, y_mid = (x_min + x_max) / 2, (y_min + y_max) / 2
+    x_left, x_right = x_mid - (x_max - x_min) * 2, x_mid + (x_max - x_min) * 2
+    y_down, y_up = y_mid - (y_max - y_min) * 2, y_mid + (y_max - y_min) * 2
+    points = np.concatenate((points, [
+        [x_left, y_mid],
+        [x_right, y_mid],
+        [x_mid, y_down],
+        [x_mid, y_up]
+    ]))
     vor = Voronoi(points)
-    print(vor.regions)
+    regions, vertices, point_regions = vor.regions, vor.vertices, vor.point_region
     polygons = []
-    for region in vor.regions:
-        if -1 not in region:
-            polygon = Polygon([vor.vertices[i] for i in region])
-            polygons.append(polygon.intersection(world))
+    for pr in point_regions:
+        if -1 in regions[pr]:
+            continue
+        v = []
+        for pt_ind in regions[pr]:
+            v.append(vertices[pt_ind])
+        v = np.array(v)
+        poly = Polygon(v)
+        # poly = poly.intersection(world)
+        polygons.append(poly)
+    if len(polygons) != len(points) - 4:
+        from matplotlib import pyplot as plt
+
+        # plot voronoi results
+        voronoi_plot_2d(vor)
+        plt.show()
+
+        assert len(polygons) == len(points) - 4
+    # print('')
+    # for ind in range(len(points) - 4):
+    #     print(f'Original point #{ind}: ', points[ind])
+    #     print('Polygon: ', polygons[ind])
     return polygons
